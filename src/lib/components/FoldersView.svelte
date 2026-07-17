291fb82c3a6392ff3d0878d1516bbb7657d0509b
Eric Soltys <ericjamessoltys@outlook.com>
2026-07-17 12:55:46 -0700

    ui: remove max-width constraint from settings content area to improve alignment

--- src/lib/components/FoldersView.svelte
+++ src/lib/components/FoldersView.svelte
@@ -205,7 +205,7 @@
   </div>
 
   <!-- Content Area -->
-  <div class="flex-1 overflow-y-auto p-6 max-w-4xl space-y-6" class:pb-24={playerStore.hasEverPlayed}>
+  <div class="flex-1 overflow-y-auto p-6 space-y-6" class:pb-24={playerStore.hasEverPlayed}>
     {#if settingsTab === "general"}
       <!-- General Settings Section -->
       <div class="bg-brand-sidebar border border-brand-border rounded-xl p-6 space-y-5">
